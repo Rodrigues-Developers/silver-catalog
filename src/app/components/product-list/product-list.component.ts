8e537835b0c86732cf735dc7d35d0d51d33f8e43
Machado21 <grmachado12@hotmail.com>
2025-02-18 06:51:43 -0400

    feat: (product-list) :sparkles: implement product filtering and update price range
    Updates: Create a filter side menu for all the items #35

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,30 +1,52 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { ApiService } from "src/app/api.service";
 import { Product } from "src/app/interfaces/products.interface";
 import { CapitalizePipe } from "src/app/shared/pipes/capitalize.pipe";
 import { Router } from "@angular/router";
+import { FilterService } from 'src/app/services/filter.service';
 
 @Component({
   selector: "app-product-list",
   imports: [CommonModule, CapitalizePipe],
   templateUrl: "./product-list.component.html",
-  styleUrl: "./product-list.component.less",
+  styleUrls: ["./product-list.component.less"],
   standalone: true,
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   hasApiError = false;
-  productsList: Product[];
+  productsList: Product[] = [];
+  filteredProductsList: Product[] = [];
 
-  constructor(private api: ApiService, private router: Router) {
+  constructor(private api: ApiService, private router: Router, private filterService: FilterService) {
     this.api.getProducts().subscribe({
       next: (res) => {
         this.hasApiError = false;
         this.productsList = res as Product[];
         //duplicate product list
         // this.productsList = [...this.productsList, ...this.productsList];
+        this.filteredProductsList = this.productsList;
       },
       error: () => (this.hasApiError = true),
+    });
+  }
+
+  ngOnInit(): void {
+    this.filterService.minPrice$.subscribe(minPrice => {
+      this.applyFilters();
+    });
+
+    this.filterService.maxPrice$.subscribe(maxPrice => {
+      this.applyFilters();
+    });
+  }
+
+  applyFilters() {
+    const minPrice = this.filterService.getMinPriceSubject().getValue();
+    const maxPrice = this.filterService.getMaxPriceSubject().getValue();
+
+    this.filteredProductsList = this.productsList.filter(product => {
+      return product.price >= minPrice && product.price <= maxPrice;
     });
   }
 
