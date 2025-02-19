15d60a4a69e76d6f445245052f8a6f41c16c0fae
Machado21 <grmachado12@hotmail.com>
2025-02-19 07:30:59 -0400

    feat: (product-list, side-bar, filter-service) :sparkles: add category filtering functionality and update price range logic. Updates Create a filter side menu for all the items #35

--- src/app/core/services/filter.service.ts
+++ src/app/core/services/filter.service.ts
@@ -5,11 +5,13 @@ import { BehaviorSubject } from "rxjs";
   providedIn: "root",
 })
 export class FilterService {
-  private minPriceSubject = new BehaviorSubject<number>(150);
+  private minPriceSubject = new BehaviorSubject<number>(50);
   private maxPriceSubject = new BehaviorSubject<number>(2500);
+  private selectedCategoriesSubject = new BehaviorSubject<string[]>([]);
 
   minPrice$ = this.minPriceSubject.asObservable();
   maxPrice$ = this.maxPriceSubject.asObservable();
+  selectedCategories$ = this.selectedCategoriesSubject.asObservable();
 
   setMinPrice(price: number) {
     this.minPriceSubject.next(price);
@@ -19,11 +21,19 @@ export class FilterService {
     this.maxPriceSubject.next(price);
   }
 
+  setSelectedCategories(categories: string[]) {
+    this.selectedCategoriesSubject.next(categories);
+  }
+
   getMinPriceSubject() {
     return this.minPriceSubject;
   }
 
   getMaxPriceSubject() {
     return this.maxPriceSubject;
+  }
+
+  getSelectedCategoriesSubject() {
+    return this.selectedCategoriesSubject;
   }
 }
