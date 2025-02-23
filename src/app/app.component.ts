2d918c1c75f7d0031b7259421debedfa58214057
Machado21 <grmachado12@hotmail.com>
2025-02-23 08:34:09 -0400

    feat: (product-list, side-bar) :sparkles: refactor product and category listing to use trackBy for improved performance Address:Create a filter side menu for all the items #35

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { NavBarComponent } from "./components/shared/nav-bar/nav-bar.component";
 import { FooterComponent } from "./components/shared/footer/footer.component";
-// import { SideBarComponent } from "./components/shared/side-bar/side-bar.component";
 
 @Component({
   selector: "app-root",
