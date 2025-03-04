c9d88e469ca5717689646578c317919f2e2c6f08
brunorcx <brunorcx@hotmail.com>
2025-03-04 15:38:49 -0400

    fix(array): removed wrong pushed file

--- src/app/utils/array.ts
+++
@@ -1,4 +0,0 @@
-// check if all elements in an array are null or undefined
-export function isAllNullOrUndefined(arr: any[]): boolean {
-  return arr.every((el) => el === null || el === undefined);
-}
