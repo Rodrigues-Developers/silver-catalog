de8adb7c1529939e5976d935ae2cf4924dd86c20
brunorcx <brunorcx@hotmail.com>
2025-03-16 08:10:18 -0400

    feat(cart): added function to send to whatsApp

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,13 @@
-import { Component, effect } from "@angular/core";
+import { Component, effect, OnDestroy } from "@angular/core";
 import { CartService } from "src/app/core/services/cart.service";
 import { MatIconModule } from "@angular/material/icon";
 import { ToastrService } from "ngx-toastr";
 import { CommonModule } from "@angular/common";
 import { Router } from "@angular/router";
 import { Cart } from "src/app/interfaces/cart.interface";
-
+import { AuthService } from "src/app/core/services/auth.service";
+import { User } from "firebase/auth";
+import { Subscription } from "rxjs";
 @Component({
   selector: "app-cart",
   standalone: true,
@@ -13,8 +15,11 @@ import { Cart } from "src/app/interfaces/cart.interface";
   templateUrl: "./cart.component.html",
   styleUrl: "./cart.component.less",
 })
-export class CartComponent {
-  constructor(public cartService: CartService, private toastr: ToastrService, private router: Router) {
+export class CartComponent implements OnDestroy {
+  currentUser: User | null = null;
+  currentUserSubscription: Subscription;
+
+  constructor(public cartService: CartService, private toastr: ToastrService, private router: Router, private authService: AuthService) {
     effect(() => {
       if (this.cartService.cartLimitReached()) {
         this.toastr.error("Você atingiu o limite máximo de items no carrinho.", "", {
@@ -22,6 +27,9 @@ export class CartComponent {
           newestOnTop: true, // fix multiple toasts stacking
         });
       }
+    });
+    this.currentUserSubscription = this.authService.user$.subscribe((user) => {
+      this.currentUser = user; // Update user state when auth state changes
     });
   }
 
@@ -31,5 +39,45 @@ export class CartComponent {
 
   redirectToItem(item: Cart) {
     this.router.navigate(["/product", item.product.id]);
+  }
+
+  onSubmit() {
+    if (!this.currentUser) {
+      this.authService.loginWithGoogle();
+      return;
+    }
+
+    const baseURL = window.location.origin;
+    const order = this.cartService.items().map((item) => {
+      return {
+        name: item.product.name,
+        price: item.product.price,
+        quantity: item.amount,
+        url: `${baseURL}/product/${item.product.id}`,
+        imageUrl: item.product.image, // Assuming each product has an 'image' property
+      };
+    });
+
+    order["total"] = this.cartService.totalSum();
+
+    // 📝 Format WhatsApp message
+    let message = ` *Novo Pedido* \n\n`;
+    order.forEach((item) => {
+      message += `*${item.name}*\n R$${item.price.toFixed(2)} x ${item.quantity}\n ${item.url}\n ${item.imageUrl}\n\n`;
+    });
+    message += ` *Total:* R$${order["total"].toFixed(2)}`;
+
+    // Encode message for URL
+    const encodedMessage = encodeURIComponent(message);
+
+    // Replace with actual recipient number
+    const phoneNumber = "559591618635";
+
+    // Open WhatsApp chat with formatted message
+    window.open(`https://wa.me/${phoneNumber}?text=${encodedMessage}`, "_blank");
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
   }
 }
