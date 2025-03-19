35a4217a4aefbb5cf42b26ee8b86ac781d67fb57
Machado21 <grmachado12@hotmail.com>
2025-03-19 09:26:31 -0400

    feat: (category-details, side-bar) :sparkles: add filter reset on component destruction and improve price slider event handling. Update styles for product list in category details. Updates Fix request in search #80

--- src/app/components/category-details/category-details.component.ts
+++ src/app/components/category-details/category-details.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { SideBarComponent } from "../shared/side-bar/side-bar.component";
 import { ProductListComponent } from "../product-list/product-list.component";
-import { CategoryListComponent } from "../category-list/category-list.component";
 import { HorizontalSliderComponent } from "../horizontal-slider/horizontal-slider.component";
 import { ApiService } from "src/app/api.service";
 import { Category } from "src/app/interfaces/category.interface";
 import { CapitalizePipe } from "../../shared/pipes/capitalize.pipe";
+import { FilterService } from "../../core/services/filter.service";
 
 @Component({
   selector: "app-category-details",
   standalone: true,
-  imports: [SideBarComponent, ProductListComponent, CategoryListComponent, HorizontalSliderComponent, CapitalizePipe],
+  imports: [SideBarComponent, ProductListComponent, HorizontalSliderComponent, CapitalizePipe],
   templateUrl: "./category-details.component.html",
   styleUrls: ["./category-details.component.less"],
 })
-export class CategoryDetailsComponent implements OnInit {
+export class CategoryDetailsComponent implements OnInit, OnDestroy {
   hasApiError = false;
   categoryList: Category[] = [];
-
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private filterService: FilterService) {}
 
   ngOnInit(): void {
     this.fetchCategories();
   }
-
+  ngOnDestroy(): void {
+    this.filterService.resetFilters();
+  }
   fetchCategories(): void {
     this.api.getCategories().subscribe({
       next: (res) => {
@@ -37,4 +38,5 @@ export class CategoryDetailsComponent implements OnInit {
     const categoryId = window.location.href.split("/").pop();
     return this.categoryList.filter((category) => category.id === categoryId)[0]?.name;
   }
+
 }
